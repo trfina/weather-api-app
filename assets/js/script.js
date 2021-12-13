eab78ccfd1d4caf6c03027a755db3f0409e396a0
Tammy Fina <finatammy@gmail.com>
2021-12-12 20:33:14 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var weather;
 function setup() {
     createCanvas(400,200);
-    loadJSON('http://api.openweathermap.org/data/2.5/forcast/daily?q=(city,state,countrY)&cnt=5&units=imperial&appid=83cbfee912c51866e75dd9168b65181f', gotData);
+    loadJSON('http://api.openweathermap.org/data/2.5/forecast/daily?q=(city,state,countrY)&cnt=5&units=imperial&appid=83cbfee912c51866e75dd9168b65181f', gotData);
 }
 
 function gotData(city,state,country) {
